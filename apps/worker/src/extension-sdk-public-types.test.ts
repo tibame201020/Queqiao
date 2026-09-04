2f3127f1b3a184ca1edeb39edd0d8b9272f46d5d
Jeff-Hsu024 <hsu024722@gmail.com>
2026-09-04 18:02:21 +0800

    test(extension): use TypeScript ESM default export

--- apps/worker/src/extension-sdk-public-types.test.ts
+++ apps/worker/src/extension-sdk-public-types.test.ts
@@ -1,7 +1,7 @@
 import { mkdtemp, rm, writeFile } from "node:fs/promises";
 import os from "node:os";
 import path from "node:path";
-import * as ts from "typescript";
+import ts from "typescript";
 import { afterEach, describe, expect, it } from "vitest";
 
 let temporary: string | undefined;
